25df2e92c5e86a73ce61d24a5e67054c5780ff4d
Quentin Stievenart <quentin.stievenart@gmail.com>
2009-08-24 11:28:27 +0200

    syntax hl added

--- paste.pl
+++ paste.pl
@@ -2,6 +2,7 @@
 use strict;
 use CGI qw/:standard/;
 use File::Basename;
+use Syntax::Highlight::Engine::Kate;
 
 my $FILENAME_LENGTH=10;
 my $PASTES_PATH="pastes/";
@@ -34,6 +35,15 @@ sub error {
   }
 }
 
+sub languagebox {
+  my @languages = (new Syntax::Highlight::Engine::Kate())->languageList();
+  my $box = "<select name=\"hl\" size=\"" . scalar(@languages) . "\">\n";
+  foreach my $lang (@languages) {
+    $box .= "<option value=\"$lang\">$lang</option>\n";
+  }
+  return $box;
+}
+
 # fill the content of the page
 sub fill {
   if (param("paste")) {
@@ -47,9 +57,9 @@ sub fill {
       return error ("Internal error", "Error when opening $path : $!");
     print FILE param("paste");
 		close FILE;
-
     return p("Your paste is located " . 
-             a({href=>basename($0) . "?id=". basename($path)}, "here"));
+             a({href=>basename($0) . "?id=". basename($path) . "&hl=" .
+               param("hl")}, "here"));
   }
   # View a paste
   elsif (param("id")) {
@@ -59,12 +69,47 @@ sub fill {
     }
     open (FILE, '<', $path) or 
       return error "Internal Error", "Error when opening : $path : $!";
-    my $content = "<code>";
+    my $content;
     while (<FILE>) {
       $content .= $_;
     }
-    $content .= "</code>";
-    return $content;
+    if (param("hl")) {
+      my $hl = new Syntax::Highlight::Engine::Kate(
+        language => "Perl",
+        substitutions => {
+           "<" => "&lt;",
+           ">" => "&gt;",
+        },
+        format_table => {
+           Alert => ["<font color=\"#0000ff\">", "</font>"],
+           BaseN => ["<font color=\"#007f00\">", "</font>"],
+           BString => ["<font color=\"#c9a7ff\">", "</font>"],
+           Char => ["<font color=\"#ff00ff\">", "</font>"],
+           Comment => ["<font color=\"#7f7f7f\"><i>", "</i></font>"],
+           DataType => ["<font color=\"#0000ff\">", "</font>"],
+           DecVal => ["<font color=\"#00007f\">", "</font>"],
+           Error => ["<font color=\"#ff0000\"><b><i>", "</i></b></font>"],
+           Float => ["<font color=\"#00007f\">", "</font>"],
+           Function => ["<font color=\"#007f00\">", "</font>"],
+           IString => ["<font color=\"#ff0000\">", ""],
+           Keyword => ["<b>", "</b>"],
+           Normal => ["", ""],
+           Operator => ["<font color=\"#ffa500\">", "</font>"],
+           Others => ["<font color=\"#b03060\">", "</font>"],
+           RegionMarker => ["<font color=\"#96b9ff\"><i>", "</i></font>"],
+           Reserved => ["<font color=\"#9b30ff\"><b>", "</b></font>"],
+           String => ["<font color=\"#ff0000\">", "</font>"],
+           Variable => ["<font color=\"#0000ff\"><b>", "</b></font>"],
+           Warning => ["<font color=\"#0000ff\"><b><i>", "</b></i></font>"],
+        },
+      );
+ 
+      if (grep { $_ eq param("hl") } $hl->languageList()) {
+        $hl->language(param("hl"));
+        $content = $hl->highlightText($content);
+      }
+    }
+    return "<pre><code>\n" . $content . "\n</pre></code>";
   }
   else {
     # The paste page 
@@ -73,6 +118,7 @@ sub fill {
                     -cols=>80, 
                     -rows=>20) .
            br . 
+           languagebox() .
            submit("Paste it §") .
            end_form();
   }
